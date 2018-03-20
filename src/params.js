b2ca224f55bd9eae7c8947809efe76100754fc38
aziz <aziz@aziz.com>
2018-03-20 14:58:57 -0400

    add offset to qs params

--- src/params.js
+++ src/params.js
@@ -50,6 +50,7 @@ module.exports = {
   }),
 
   queryStringParams: Object.freeze({
+    'offset': numeric,
     'state': contains.bind( null, ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'] ),
     'district': function ( num ) {
       var num = Number( num );
